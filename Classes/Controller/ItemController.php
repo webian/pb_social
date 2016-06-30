6fac451373581d6823d63280461b05b476b433d9
Ramon Mohi <rm@plusb.de>
2016-06-30 14:28:31 +0200

    added essential setting for ajax post fetch

--- Classes/Controller/ItemController.php
+++ Classes/Controller/ItemController.php
@@ -138,7 +138,10 @@ class ItemController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController 
         $this->logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Log\LogManager')->getLogger(__CLASS__);
 
         // update feedRequestLimit if request is asynchronous
-        if($ajax) $this->settings['feedRequestLimit'] = $this->settings['asynchLimit'] > 0 ? $this->settings['asynchLimit'] : $this->settings['feedRequestLimit'];
+        if($ajax){
+            $this->settings['feedRequestLimit'] = $this->settings['asynchLimit'] > 0 ? $this->settings['asynchLimit'] : $this->settings['feedRequestLimit'];
+            $this->settings['devMod'] = 1;
+        }
 
         # Get feeds #
         $feeds = array();
