613b7b67f43a90a260c36c2fdb89d86667f7baf4
Arend Maubach <arend.maubach@plusb.de>
2019-05-07 11:58:52 +0200

    simlpify extension registration

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,64 +3,53 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-    'PlusB.' . $_EXTKEY,
-    'Socialfeed',
-    array(
-        'Item' => 'showSocialFeed',
-    ),
-    // non-cacheable actions
-    array(
-        'Item' => 'showSocialFeed',
-    )
-);
 
-if(TYPO3_MODE === 'BE') {
-    // Constants
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY,'constants',' <INCLUDE_TYPOSCRIPT: source="FILE:EXT:'. $_EXTKEY .'/Configuration/TypoScript/constants.txt">');
-    // Setup
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript($_EXTKEY,'setup',' <INCLUDE_TYPOSCRIPT: source="FILE:EXT:'. $_EXTKEY .'/Configuration/TypoScript/setup.txt">');
+call_user_func(function () {
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'PlusB\\PbSocial\\Command\\PBSocialCommandController';
-
-
-    // wizards
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        'mod {
-                wizards.newContentElement.wizardItems.plugins {
-                    elements {
-                        socialfeed {
-                            iconIdentifier = pb_social-plugin-socialfeed
-                            title = LLL:EXT:'. $_EXTKEY .'/Resources/Private/Language/de.locallang_db.xlf:socialfeed_wizard.title
-                            description = LLL:EXT:'. $_EXTKEY .'/Resources/Private/Language/de.locallang_db.xlf:socialfeed_wizard.description
-                            tt_content_defValues {
-                                CType = list
-                                list_type = pbsocial_socialfeed
-                            }
-                        }
-                    }
-                    show = *
-                }
-           }'
-    );
-
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    $iconRegistry->registerIcon(
-            'pb_social-plugin-socialfeed',
-            \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-            ['source' => 'EXT:'. $_EXTKEY .'/Resources/Public/Icons/Extension.svg']
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+            'PlusB.PbSocial',
+            'Socialfeed',
+            array(
+                'Item' => 'showSocialFeed',
+            ),
+            // non-cacheable actions
+            array(
+                'Item' => 'showSocialFeed',
+            )
         );
-}
 
-/**
- * register cache
- */
-// Register cache frontend for proxy class generation
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['pb_social_cache'] = array(
-    'groups' => array(
-        'system'
-    ),
-    'options' => array(
-        'defaultLifetime' => 3600,
-    )
-);
+        if(TYPO3_MODE === 'BE') {
+            // Constants
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('pb_social','constants',' <INCLUDE_TYPOSCRIPT: source="FILE:EXT:pb_social/Configuration/TypoScript/constants.txt">');
+            // Setup
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScript('pb_social','setup',' <INCLUDE_TYPOSCRIPT: source="FILE:EXT:pb_social/Configuration/TypoScript/setup.txt">');
+
+            $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'PlusB\\PbSocial\\Command\\PBSocialCommandController';
+
+            // Include new content elements to modWizards
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+                '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:pb_social/Configuration/PageTSconfig/PbSocialSocialfeed.ts">'
+            );
+
+            $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
+            $iconRegistry->registerIcon(
+                    'pbsocial_socialfeed',
+                    \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+                    ['source' => 'EXT:pb_social/Resources/Public/Icons/Extension.svg']
+                );
+        }
+
+        /**
+         * register cache
+         */
+        // Register cache frontend for proxy class generation
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['pb_social_cache'] = array(
+            'groups' => array(
+                'system'
+            ),
+            'options' => array(
+                'defaultLifetime' => 3600,
+            )
+        );
+
+});
--- ext_tables.php
+++ ext_tables.php
@@ -3,13 +3,15 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
+
+
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-    'PlusB.' .$_EXTKEY,
+    'PlusB.PbSocial',
     'Socialfeed',
     'SocialFeed'
 );
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Social Media Stream');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('pb_social', 'Configuration/TypoScript', 'Social Media Stream');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pbsocial_domain_model_item', 'EXT:pb_social/Resources/Private/Language/locallang_csh_tx_pbsocial_domain_model_item.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pbsocial_domain_model_item');
@@ -18,10 +20,8 @@ if (!defined('TYPO3_MODE')) {
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_pbsocial_domain_model_credential', 'EXT:pb_social/Resources/Private/Language/locallang_csh_tx_pbsocial_domain_model_credential.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_pbsocial_domain_model_credential');
 
-$extensionName = strtolower(\TYPO3\CMS\Core\Utility\GeneralUtility::underscoredToUpperCamelCase($_EXTKEY));
+$pluginSignature_feed = 'pbsocial_socialfeed';
 
-$pluginName_feed = strtolower('Socialfeed');
-$pluginSignature_feed = $extensionName . '_' . $pluginName_feed;
 $TCA['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature_feed] = 'layout,select_key,pages,recursive';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature_feed] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_feed, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForms/socialfeed.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature_feed, 'FILE:EXT:pb_social/Configuration/FlexForms/socialfeed.xml');
